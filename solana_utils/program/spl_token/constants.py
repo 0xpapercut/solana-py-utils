b25f3a2455d519d564625a55cedd5e23a2385eb4
0xpapercut <0xpapercut@gmail.com>
2024-08-28 14:50:41 +0200

    Add WRAPPED_SOL_MINT constant

--- solana_utils/program/spl_token/constants.py
+++ solana_utils/program/spl_token/constants.py
@@ -1,3 +1,4 @@
 from solders.pubkey import Pubkey
 
 TOKEN_PROGRAM_ID = Pubkey.from_string('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
+WRAPPED_SOL_MINT = Pubkey.from_string('So11111111111111111111111111111111111111112')
