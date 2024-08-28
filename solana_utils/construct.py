c9d74d5b7001469ce2d208c84cbdd5bb44074024
0xpapercut <0xpapercut@gmail.com>
2024-08-28 22:13:17 +0200

    Update construct.py

--- solana_utils/construct.py
+++ solana_utils/construct.py
@@ -1,38 +1,27 @@
-__all__ = ['Pubkey', 'String', 'Option', 'Vec']
+__all__ = ['Pubkey', 'String', 'Option', 'Vec', 'StringWithLength']
 
 from construct import (
-    Construct, Struct, ExprAdapter, PrefixedArray, GreedyBytes, Prefixed, Array, Byte,
+    Struct, ExprAdapter, PrefixedArray, GreedyBytes,
+    Prefixed, Array, Byte, Bytes,
     Int32ul, Int8ul,
     If, this,
 )
 from solders.pubkey import Pubkey as SoldersPubkey
 
-PUBKEY_LENGTH: 32
-
-def _singleton(arg):
-    x = arg()
-    return x
-
-@_singleton
-class Pubkey(Construct):
-    def _parse(self, stream, context, path):
-        return SoldersPubkey(stream.read(32))
-
-    def _build(self, obj, stream, context, path):
-        if not isinstance(obj, SoldersPubkey):
-            raise TypeError("Expected a solders.pubkey.Pubkey object")
-
-    def _sizeof(self, context, path):
-        return PUBKEY_LENGTH
-
-# Pubkey = ExprAdapter(
-#     Array(32, Byte),
-#     encoder=lambda obj, ctx: bytes(obj),
-#     decoder=lambda obj, ctx: SoldersPubkey(obj),
-# )
+Pubkey = ExprAdapter(
+    Array(32, Byte),
+    encoder=lambda obj, ctx: bytes(obj),
+    decoder=lambda obj, ctx: SoldersPubkey(obj),
+)
 
 String = ExprAdapter(
     Prefixed(Int32ul, GreedyBytes),
+    encoder=lambda obj, ctx: obj.encode('utf-8'),
+    decoder=lambda obj, ctx: obj.decode('utf-8'),
+)
+
+StringWithLength = lambda length: ExprAdapter(
+    Prefixed(Int32ul, Bytes(length)),
     encoder=lambda obj, ctx: obj.encode('utf-8'),
     decoder=lambda obj, ctx: obj.decode('utf-8'),
 )
