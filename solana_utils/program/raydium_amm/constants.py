6f29e0757c75478e5b5bc4dd50db6ddc87db2316
0xpapercut <0xpapercut@gmail.com>
2024-08-28 18:11:38 +0200

    Update Raydium constants

--- solana_utils/program/raydium_amm/constants.py
+++ solana_utils/program/raydium_amm/constants.py
@@ -1,1 +1,3 @@
-RAYDIUM_AMM_PROGRAM_ID = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8"
+from solders.pubkey import Pubkey
+
+RAYDIUM_AMM_PROGRAM_ID = Pubkey.from_string("675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8")
