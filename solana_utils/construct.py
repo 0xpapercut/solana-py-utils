30682c83414daf41fc80890101c1a04f18fc9add
0xpapercut <0xpapercut@gmail.com>
2024-08-28 18:07:05 +0200

    Update construct utilities

--- solana_utils/construct.py
+++ solana_utils/construct.py
@@ -1,4 +1,10 @@
-from construct import Construct
+__all__ = ['Pubkey', 'String', 'Option', 'Vec']
+
+from construct import (
+    Construct, Struct, ExprAdapter, PrefixedArray, GreedyBytes, Prefixed, Array, Byte,
+    Int32ul, Int8ul,
+    If, this,
+)
 from solders.pubkey import Pubkey as SoldersPubkey
 
 PUBKEY_LENGTH: 32
@@ -18,3 +24,26 @@ class Pubkey(Construct):
 
     def _sizeof(self, context, path):
         return PUBKEY_LENGTH
+
+# Pubkey = ExprAdapter(
+#     Array(32, Byte),
+#     encoder=lambda obj, ctx: bytes(obj),
+#     decoder=lambda obj, ctx: SoldersPubkey(obj),
+# )
+
+String = ExprAdapter(
+    Prefixed(Int32ul, GreedyBytes),
+    encoder=lambda obj, ctx: obj.encode('utf-8'),
+    decoder=lambda obj, ctx: obj.decode('utf-8'),
+)
+
+Option = lambda struct: ExprAdapter(
+    Struct(
+        "is_some" / Int8ul,
+        "value" / If(this.is_some == 1, struct)
+    ),
+    encoder=lambda obj, ctx: {'is_some': 1 if obj else 0, 'value': obj},
+    decoder=lambda obj, ctx: obj['value'] if obj['is_some'] == 1 else None,
+)
+
+Vec = lambda struct: PrefixedArray(Int32ul, struct)
